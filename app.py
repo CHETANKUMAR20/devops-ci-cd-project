ac50b433dec038368eb6558af8b8ab532d9a4ba3
CHETANKUMAR20 <c458047@gmail.com>
2026-02-19 21:52:36 +0530

    Fixed indentation error

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-'''    return "DevOps CI/CD Project Running 🚀" '''
     return "Version 2 – Automated Deployment 🚀"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
